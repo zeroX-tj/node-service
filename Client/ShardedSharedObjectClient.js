ddd24c06c7e923aa899679b4c126267f446abbed
Tim Janssens <tim@emilioit.com>
2016-10-13 13:34:34 +0200

    disable debug

--- Client/ShardedSharedObjectClient.js
+++ Client/ShardedSharedObjectClient.js
@@ -17,11 +17,11 @@ class ShardedSharedObjectClient{
                 self.service[endpoint.name].subscribe(channel);
             });
             this.service[endpoint.name].on('init', ()=>{
-                console.log('init')
+                //console.log('init')
                 bridge.endpoints[endpoint.name].emit('_init')
             });
             this.service[endpoint.name].on('update', (d)=>{
-                console.log('update',JSON.stringify(d))
+                //console.log('update',JSON.stringify(d))
                 bridge.endpoints[endpoint.name].emit('update', d)
             });
         })
