1b1fbd94236b266a4276f2eda01f92cbbab2fec0
Tim Janssens <tim@emilioit.com>
2016-10-14 20:24:30 +0200

    cleaner worker code

--- misc/worker.js
+++ misc/worker.js
@@ -2,26 +2,39 @@
 require('json.date-extensions');
 JSON.useDateParser();
 var id = process.argv[2];
-var init_data = JSON.parse(process.argv[3]);
+var initial_data = JSON.parse(process.argv[3]);
 var descriptor = JSON.parse(process.argv[4]);
 var worker = require(process.argv[5]);
 var _ = require('underscore');
-
-if(!(_.isFunction(worker.init) && _.isFunction(worker.put) && _.isFunction(worker.remove)))
-    throw new Error('worker should implement init, put, remove');
-
-var service = worker.init(descriptor, init_data);
-
+var nodeservice = require("../index");
+var service = new nodeservice.Service(descriptor, worker.handlers, initial_data);
+worker.data = initial_data;
 process.on('message', (payload)=>{
     switch (payload.cmd){
         case 'put':
             var path = payload.data.key;
             var endpointName = path.shift();
             //console.log(data)
-            worker.put(endpointName, path, payload.data.value);
+            var to = service[endpointName].data;
+            var field = path.pop();
+            path.forEach((k)=>{
+                if(!to[k])
+                    to[k]={};
+                to = to[k];
+            });
+            to[field] = payload.data.value;
+            service[endpointName].notify(path);
             break;
         case 'remove':
-            worker.remove(payload.data);
+            var to = service[endpointName].data;
+            var field = path.pop();
+            path.forEach((k)=>{
+                if(!to[k])
+                    return;
+                to = to[k];
+            });
+            delete to[field];
+            service[endpointName].notify(path);
             break;
         case 'rpc':
             service.RPCServices[payload.data.endpointName].handler(payload.data.req, (err, res)=>{
--- test/SSO_Worker.js
+++ test/SSO_Worker.js
@@ -1,35 +1,9 @@
 "use strict";
-var service = require("../index");
-var s;
-var data = {};
 module.exports = {
-    init: (descriptor,initial_data)=>{
-        s = new service.Service(descriptor, {TakeSnapshot: (req, rep)=>{
-            rep(null, {w00t:'t00t'})
-        }}, initial_data);
-        data = initial_data;
-        return s;
-    },
-    put: (endpointName, path, value)=>{
-        var to = s[endpointName].data;
-        var field = path.pop();
-        path.forEach((k)=>{
-            if(!to[k])
-                to[k]={};
-            to = to[k];
-        });
-        to[field] = value;
-        s[endpointName].notify(path);
-    },
-    remove: (endpointName, path)=>{
-        var to = s[endpointName].data;
-        var field = path.pop();
-        path.forEach((k)=>{
-            if(!to[k])
-                return;
-            to = to[k];
-        });
-        delete to[field];
-        s[endpointName].notify(path);
-    },
+    data: {},
+    handlers: {
+        TakeSnapshot: (req, rep) => {
+            rep(null, {w00t: 't00t'})
+        }
+    }
 };
